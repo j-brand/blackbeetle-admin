4c15a3a8da14da0c23d64baa42bf075868f3f2f4
johbra90 <johbra90@bitbucket.org>
2021-02-05 18:24:12 +0100

    environment file clean added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: '{API URL}',
+  publicUrl: '{PUBLIC URL}',
+  csrf: '{API URL FOR CSRF}',
+  tiny_mce_key: '{TINY MCE KEY}',
+  google_api_key: '{GOOGLE API KEY}',
+};
