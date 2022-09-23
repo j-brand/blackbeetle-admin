164f96bde34710115bbc3cabf0618bcfbf169724
ToyMaker <johannes@blackbeetle.de>
2022-09-23 13:23:08 +0000

    conf for github actions

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+const backgendURL = `${process.env.BACKEND_URL}`;
+
+export const environment = {
+  production: false,
+  apiUrl: `${backgendURL}/admin`,
+  publicUrl: `${backgendURL}`,
+  csrf: `${backgendURL}/sanctum/csrf-cookie`,
+  tiny_mce_key: `${process.env.TINY_MCE_KEY}`,
+  google_api_key: `${process.env.GOOGLE_API_KEY}`,
+};
