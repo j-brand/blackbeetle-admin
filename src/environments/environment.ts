dd155cf974f0d51def81eb5486eb3c2989b32a58
ToyMaker <johannes@blackbeetle.de>
2022-09-28 14:58:16 +0000

    env confugured

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 const backgendURL = '{BACKEND_URL}';
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: `${backgendURL}/admin`,
   publicUrl: `${backgendURL}`,
   csrf: `${backgendURL}/sanctum/csrf-cookie`,
-  tiny_mce_key: '{process.env.TINY_MCE_KEY}',
-  google_api_key: '{process.env.GOOGLE_API_KEY}',
+  tiny_mce_key: '{TINY_MCE_KEY}',
+  google_api_key: '{GOOGLE_API_KEY}',
 };
